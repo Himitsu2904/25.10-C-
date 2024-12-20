910102cfe9fc91f70635f4898d6f489b2dd4d0cb
alexandrachernenko2006@gmail.com <alexandrachernenko2006@gmail.com>
2024-12-20 12:28:18 +0200

    Add project files.

---
+++ Class.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class YearException : ApplicationException
+    {
+        public YearException(): base("Некорректный год.") { }
+    }
+
+    public class MonthException: ApplicationException
+    {
+        public MonthException() : base("Некорректный месяц.") { }
+    }
+
+    public class DayException: ApplicationException
+    {
+        public DayException() : base("Некорректное число месяца.") { }
+    }
+
+    internal class Date
+    {
+        private int year;
+        private int month;
+        private int day;
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new YearException();
+                }
+                year = value;
+            }
+        }
+        public int Month
+        {
+            get { return month; }
+            set
+            {
+                if (value < 1 || value > 12)
+                {
+                    throw new MonthException();
+                }
+                month = value;
+            }
+        }
+        public int Day
+        {
+            get { return day; }
+            set
+            {
+                if (value < 1 || value > 31)
+                {
+                    throw new DayException();
+                }
+                day = value;
+            }
+        }
+        public Date()
+        {
+            year = 1970;
+            month = 1;
+            day = 1;
+        }
+        public Date(int y, int m, int d)
+        {
+            if (y < 1)
+            {
+                throw new YearException();
+            }
+            if (m < 1 || m > 12)
+            {
+                throw new MonthException();
+            }
+            if (d < 1 || d > 31)
+            {
+                throw new DayException();
+            }
+            year = y;
+            month = m;
+            day = d;
+        }
+        public long DifferenceBetween(int y, int m, int d)
+        {
+            long currentDays = (long)(year * 365 + month * 30.5 + day);
+            long inputedDays = (long)(y * 365 + m * 30.5 + d);
+            if (inputedDays > currentDays)
+            {
+                return inputedDays - currentDays;
+            }
+            else return currentDays - inputedDays;
+        }
+        public Date ChangeDate(int d)
+        {
+            long dateInDays = (long)(year * 365 + month * 30.5 + day);
+            dateInDays += d;
+            year = (int)(dateInDays / 365);
+            dateInDays -= (long)(year * 365);
+            month = (int)(dateInDays / 30.5);
+            dateInDays -= (long)(month * 30.5);
+            day = (int)(dateInDays);
+            return new Date(year, month, day);
+        }
+        public void ShowDate()
+        {
+            if (day < 10)
+            {
+                Console.Write($"0{day}.");
+            }
+            else Console.Write(day + ".");
+            if (month < 10)
+            {
+                Console.Write($"0{month}.");
+            }
+            else Console.Write(month + ".");
+            Console.Write(year);
+        }
+    }
+
+    internal class Employee
+    {
+        private string FIO;
+        private string birthday;
+        private string phoneNumber;
+        private string email;
+        private string position;
+        private string[] duties = new string[5];
+
+        public Employee() { }
+
+        public void InputData()
+        {
+            Console.WriteLine("ВВЕДИТЕ ДАННЫЕ ПРО СОТРУДНИКА:");
+            Console.Write("ФИО: ");
+            FIO = Console.ReadLine();
+            Console.Write("Дата рождения: ");
+            birthday = Console.ReadLine();
+            Console.Write("Номер телефона: ");
+            phoneNumber = Console.ReadLine();
+            Console.Write("email: ");
+            email = Console.ReadLine();
+            Console.Write("Должность: ");
+            position = Console.ReadLine();
+            Console.Write("Служебные обязанности (5):");
+            for (int i = 0; i < duties.Length; i++)
+            {
+                duties[i] = Console.ReadLine();
+            }
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("---ИНФОРМАЦИЯ О СОТРУДНИКЕ---");
+            Console.WriteLine($"ФИО: {FIO}\nДата рождения: {birthday}\nНомер Телефона: {phoneNumber}");
+            Console.WriteLine($"email: {email}\nДолжность: {position}");
+            Console.WriteLine("Служебные обязанности:");
+            foreach (string d in duties)
+            {
+                Console.WriteLine("> " + d);
+            }
+        }
+
+        public string GetFIO() => FIO;
+        public string GetBirthday() => birthday;
+        public string GetPhoneNumber() => phoneNumber;
+        public string GetEmail() => position;
+        public string GetPosition() => position;
+        public string[] GetDuties() => duties;
+        public string GetDuties(int index) => duties[index];
+
+        public void SetPhoneNumber(string value) => phoneNumber = value;
+        public void SetEmail(string value) => email = value;
+        public void SetPosition(string value) => position = value;
+        public void SetDuties(string[] value) => duties = value;
+        public void SetDuties(int index, string value) => duties[index] = value;
+    }
+
+    internal class Plane
+    {
+        private string name;
+        private string production;
+        private string type;
+        private int year;
+
+        public Plane() { }
+
+        public Plane(string n, string p, string t, int y)
+        {
+            name = n;
+            production = p;
+            type = t;
+            year = y;
+        }
+
+        public void InputData()
+        {
+            Console.WriteLine("ВВЕДИТЕ ДАННЫЕ ПРО СОТРУДНИКА:");
+            Console.Write("Название: ");
+            name = Console.ReadLine();
+            Console.Write("Производитель: ");
+            production = Console.ReadLine();
+            Console.Write("Тип: ");
+            type = Console.ReadLine();
+            Console.Write("Год выпуска: ");
+            year = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("---ИНФОРМАЦИЯ О САМОЛЁТЕ---");
+            Console.WriteLine($"Название: {name}\nПроизводитель: {production}\nТип: {type}\nГод выпуска: {year}");
+        }
+
+        public string GetName() => name;
+        public string GetProduction() => production;
+        public string GetPlainType() => type;
+        public int GetYear() => year;
+
+        public void SetName(string value) => name = value;
+        public void SetProduction(string value) => production = value;
+        public void SetPlainType(string value) => type = value;
+        public void SetYear(int value) => year = value;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Class;
+
+class Program
+{
+    #region Функции для шифра Цезаря
+    static string Encrypt(string str, int shift = 3)
+    {
+        StringBuilder newStr = new StringBuilder();
+        newStr.Append(str);
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsLetter(str[i]))
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    newStr[i] = char.ToLower(newStr[i]);
+                }
+                newStr[i] += (char)shift;
+                if (newStr[i] > 'z')
+                {
+                    newStr[i] -= (char)26;
+                }
+                else if (newStr[i] < 'a')
+                {
+                    newStr[i] += (char)26;
+                }
+                if (char.IsUpper(str[i]))
+                {
+                    newStr[i] = char.ToUpper(newStr[i]);
+                }
+            }
+        }
+        str = newStr.ToString();
+        return str;
+    }
+    static string Decrypt(string str, int shift = 3)
+    {
+        return Encrypt(str, 26 - shift);
+    }
+
+    static string SlavicEncrypt(string str, int shift = 3)
+    {
+        StringBuilder newStr = new StringBuilder();
+        newStr.Append(str);
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (char.IsLetter(str[i]))
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    newStr[i] = char.ToLower(newStr[i]);
+                }
+                newStr[i] += (char)shift;
+                if (newStr[i] > 'я')
+                {
+                    newStr[i] -= (char)33;
+                }
+                else if (newStr[i] < 'а')
+                {
+                    newStr[i] += (char)33;
+                }
+                if (char.IsUpper(str[i]))
+                {
+                    newStr[i] = char.ToUpper(newStr[i]);
+                }
+            }
+        }
+        str = newStr.ToString();
+        return str;
+    }
+    static string SlavicDecrypt(string str, int shift = 3)
+    {
+        return SlavicEncrypt(str, 33 - shift);
+    }
+    #endregion
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        #region 3. Шифр Цезаря
+        Console.WriteLine("What is the language of your message?");
+        Console.WriteLine("a) English\t b)Ukrainian/Russian");
+        char lang = Convert.ToChar(Console.ReadLine());
+        Console.Write("Enter string: ");
+        string str = Console.ReadLine();
+        if (lang == 'a')
+        {
+            str = Encrypt(str);
+            Console.WriteLine("Encrypted sentence: " + str);
+            str = Decrypt(str);
+        }
+        else if (lang == 'b')
+        {
+            str = SlavicEncrypt(str);
+            Console.WriteLine("Encrypted sentence: " + str);
+            str = SlavicDecrypt(str);
+        }
+        Console.WriteLine("Decrypted sentence: " + str);
+        #endregion
+
+        #region 5. Подсчет введеного арифметического выражения
+        str = null;
+        Console.Write("Enter arithmethic problem: ");
+        str = Console.ReadLine();
+        string[] problemElements = str.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        double result = Convert.ToDouble(problemElements[0]);
+        for (int i = 1; i < problemElements.Length; i+=2)
+        {
+            if (problemElements[i] == "+")
+            {
+                result += Convert.ToDouble(problemElements[i + 1]);
+            }
+            else if (problemElements[i] == "-")
+            {
+                result -= Convert.ToDouble(problemElements[i + 1]);
+            }
+        }
+        Console.WriteLine("Calculation result = " + result);
+        #endregion
+
+        #region 6. Замена первой буквы на верхний регистр
+        str = null;
+        Console.WriteLine("Enter text:");
+        str = Console.ReadLine();
+        string[] sentences = str.Split(".".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        StringBuilder temp = new StringBuilder();
+        str = null;
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            sentences[i] = sentences[i].TrimStart();
+            temp.Append(sentences[i]);
+            temp[0] = char.ToUpper(temp[0]);
+            sentences[i] = temp.ToString();
+            str += (sentences[i] + ". ");
+            temp.Clear();
+        }
+        Console.WriteLine(str);
+        #endregion
+
+        #region 7. Проверка на недопустимые слова
+        str = null;
+        str = @"
+        To be, or not to be, that is the question,
+        Whether 'tis nobler in the mind to suffer
+        The slings and arrows of outrageous fortune,
+        Or to take arms against a sea of troubles,
+        And by opposing end them? To die: to sleep;
+        No more; and by a sleep to say we end
+        The heart-ache and the thousand natural shocks
+        That flesh is heir to, 'tis a consummation
+        Devoutly to be wish'd. To die, to sleep";
+        Console.WriteLine(str);
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Write("Which word will be banned? ");
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+        string banned = Console.ReadLine();
+        int length = banned.Length;
+        string[] words = str.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        temp.Clear();
+        for (int j = 0; j < length; j++)
+        {
+            temp.Append('*');
+        }
+        for (int i = 0; i < words.Length; i++)
+        {
+            if ( !(words[i].Contains(' ')) )
+            {
+                if (words[i] == banned)
+                {
+                    words[i] = temp.ToString();
+                }
+            }
+        }
+        temp.Clear();
+        for (int i = 0; i < words.Length; i++)
+        {
+            temp.Append(words[i] + " ");
+        }
+        str = null;
+        str = temp.ToString();
+        Console.WriteLine("Censored text:" + str);
+        #endregion
+        Console.WriteLine();
+        
+        #region Демонстрация класса Date
+        //try
+        //{
+        //    Date date = new Date(2022, 7, 5);
+        //}
+        //catch(Exception e)
+        //{
+        //    Console.WriteLine(e.Message);
+        //}
+        //date.ShowDate();
+        //Console.WriteLine();
+        
+        //Console.Write("Changed date: ");
+        //date.ChangeDate(47);
+        //date.ShowDate();
+        //Console.WriteLine();
+
+        //Console.Write("Difference betweeen 12.11.2022 and ");
+        //date.ShowDate();
+        //Console.WriteLine(" = " + date.DifferenceBetween(2022, 11, 12));
+        //Console.WriteLine();
+        #endregion
+
+        #region Демонстрация класса Employee
+        Employee employee = new Employee();
+        employee.InputData();
+        employee.ShowData();
+        Console.WriteLine();
+        #endregion
+
+        #region Демонстрация класса Plane
+        Plane plane = new Plane();
+        plane.InputData();
+        plane.ShowData();
+        #endregion
+    }
+}
